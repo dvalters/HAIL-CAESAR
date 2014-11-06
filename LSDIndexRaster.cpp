2951fd238da5184018397e2eb9308f00218577d9
Stuart Grieve <s.grieve@ed.ac.uk>
2014-11-06 15:05:33 +0000

    Changed the floating point comparison to test for square dem cells in ENVI raster into a string comparison as comparing floats is messy and was flagging uneven cell dimensions where they are in fact equivalent.

--- LSDIndexRaster.cpp
+++ LSDIndexRaster.cpp
@@ -455,7 +455,9 @@ void LSDIndexRaster::read_raster(string filename, string extension)
             // get Y minium
             YMinimum = YMax - NRows*DataResolution;	          
 
-	          if (atof(mapinfo_strings[6].c_str()) != DataResolution)
+	          //using a string comparison as float(X) != float(X) in many cases due to floating point math
+	          // http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm  - SWDG	          
+	          if (mapinfo_strings[5] != mapinfo_strings[6]) 
 	          {
 	            cout << "Warning! Loading ENVI DEM, but X and Y data spacing are different!" << endl;
 	          }
