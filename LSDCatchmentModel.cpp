36b28a595ec1ad618689c5ee24abc0d036c8a647
Declan Valters <declan.valters@gmail.com>
2015-03-05 18:08:12 +0000

    Added the routine to save and output files aftern the model run (UNFINISHED)

--- LSDCatchmentModel.cpp
+++ LSDCatchmentModel.cpp
@@ -808,6 +808,150 @@ void LSDCatchmentModel::call_slide5()   // not exactly sure what slide_5 does di
 //=-=-=-=-=-=-=-=-=-=
 void LSDCatchmentModel::output_data()	  // this was part of erodep() in CL but I felt it should have its own method call - DAV
 {
+	int n;
+	Qw+newvol += temptotal*((cycle-previous)*60); // 60 seconds per min
+	
+	for (int nn == 1; nn <= rfnum; nn++) Jw_newvol += (j_mean[nn]*DX*DX*nActualGridCells[nn]) * ((cycle - previous)*60);
+	
+	// Catch all the timesteps that pass one or more hour marks
+	if ((new_cycle < old_cycle) || (cycle - previous >= output_file_save_interval))
+	{
+		while (( tx > previous ) && (cycle >= tx))
+		{
+			hours++;
+			
+			// Step 1: Calculate hourly total sediment Q (m3)
+			Qs_step = globasediq - old_sediq;
+			Qs_over = Qs_step*((cycle - tx)/(cycle - tslastcalc));
+			Qs_hour = Qs_step - Qs_over + Qs_last;
+			
+			// reset Qs_last and old_sediq for large time steps
+			if (cycle >= tx + output_file_save_interval)
+			{
+				Qs_last = 0;
+				old_sediq = globalsediq - Qs_over;
+			}
+			
+			// reset Qs_last and old_sediq for small time steps
+			if (cycle<tx+output_file_save_interval)
+			{
+				Qs_last = Qs_over;
+				old_sediq = globalsediq;
+			}
+
+			// Step 2: Calculate grain size Qgs, also calculate contaminated amounts
+			for (n=1; n<=G_MAX-1; n++)
+			{
+				// calculate timestep Qgs
+				Qg_step[n] = sum_grain[n] - old_sum_grain[n];
+				Qg_step2[n] = sum_grain2[n] - old_sum_grain2[n];
+				// Interpolate Qgs beyond time tx
+				Qg_over[n] = Qg_step[n]*((cycle - tx)/(cycle - tlastcalc));
+				Qg_over2[n] = Qg_step2[n] * ((cycle - tx) / (cycle - tlastcalc));
+				// and calculate hourly Qgs
+				Qg_hour[n] = Qg_step[n] - Qg_over[n] + Qg_last[n];
+				Qg_hour2[n] = Qg_step2[n] - Qg_over2[n] + Qg_last2[n];
+				// Reset Qg_last[n] and old_sum_grain[n]for large time steps
+				if (cycle >= tx + output_file_save_interval)
+				{
+					Qg_last[n] = 0;
+					Qg_last2[n] = 0;
+					old_sum_grain[n] = sum_grain[n] - Qg_over[n];
+					old_sum_grain2[n] = sum_grain2[n] - Qg_over2[n];
+				}
+				// Reset Qg_last[n] and old_sum_grain[n] for small time steps
+				if (cycle < tx + output_file_save_interval)
+				{
+					Qg_last[n] = Qg_over[n];
+					Qg_last2[n] = Qg_over2[n];
+					old_sum_grain[n] = sum_grain[n];
+					old_sum_grain2[n] = sum_grain2[n];
+				}
+			}
+	
+			// Step 3: Calculate hourly mean water discharge
+			// Qw_overvol = temptotal*((cycle-tx)*output_file_save_interval); // replaced by line below MJ 25/01/05
+			Qw_overvol = temptotal*((cycle - tx)*60);	// 60 secs per min
+			Qw_stepvol = Qw_newvol - Qw_oldvol;
+			Qw_hourvol = Qw_stepvol - Qw_overvol + Qw_lastvol;
+			Qw_hour = Qw_hourvol/(60*output_file_save_interval); // convert hourly water volume to cumecs
+
+			// same for Jw (j_mean contribution)  MJ 14/03/05
+			for (int nn=1; nn<=rfnum; nn++) 
+			{
+				Jw_overvol = (j_mean[nn]*DX*DX*nActualGridCells[nn])*((cycle - tx)*60);  // fixed MJ 29/03/05
+			}
+			Jw_stepvol = Jw_newvol - Jw_oldvol;
+			Jw_hourvol = Jw_stepvol - Jw_overvol + Jw_lastvol;
+			Jw_hour = Jw_hourvol/(60*output_file_save_interval);
+
+
+			// reset Qw_lastvol and Qw_oldvol for large time steps
+			if (cycle >= tx + output_file_save_interval)
+			{
+				Qw_lastvol = 0;
+				Qw_oldvol = Qw_newvol - Qw_overvol;
+
+				// same for Jw (j_mean contribution)  MJ 14/03/05
+				Jw_lastvol = 0;
+				Jw_oldvol = Jw_newvol - Jw_overvol;
+			}
+
+			// reset Qw_lastvol and Qw_oldvol for small time steps
+			if (cycle < tx + output_file_save_interval)
+			{
+				Qw_lastvol = Qw_overvol;
+				Qw_oldvol = Qw_newvol;
+
+				// same for Jw (j_mean contribution)  MJ 14/03/05
+				Jw_lastvol = Jw_overvol;
+				Jw_oldvol = Jw_newvol;
+			}
+
+			Tx = tx;
+			tx = Tx + output_file_save_interval;
+		  
+	
+			// Step 4: Output hourly data to file (format for reach model input)
+			// changed MJ 18/01/05
+			string output = string.Format("{0}",hours);
+			output = output	+ string.Format(" {0:F6}",Qw_hour);
+			output = output	+ string.Format(" {0:F6}",Jw_hour);
+			
+			// DAV: Took out the SIBERIA sub-model bit here.
+			
+			output = output + string.Format(" {0:F6}", sand_out);
+			sand_out = 0;
+
+			output = output	+ string.Format(" {0:F10}", Qs_hour);
+			
+			for (n=1; n<=G_MAX-1; n++)
+			{
+				output = output + string.Format(" {0:F10}", Qg_hour[n]);
+				//output = output+" "+Qg_hour[n]; 
+			}
+			
+			// Open the catchment time series file in append mode (ios_base::app)
+			// Open it in write mode (ios_base::out)
+			// CATCH_FILE is called "catchment.dat" by default (see the .hpp file)
+			std::ofstream timeseriesf(CATCH_FILE, std::ios_base::app | std::ios_base::out);
+			
+			// write the current timestep output to the time series file
+			timeseriesf << output << std::endl; 
+			
+			//close the file, although should you really do this if just opening it again in the next loop?
+			timeseriesf.close();
+			
+			////C# version of the above
+			//StreamWriter sw = File.AppendText(CATCH_FILE);
+			//sw.WriteLine(output);
+			//sw.Close();
+
+
+		}
+		tlastcalc = cycle;	
+	}													   
+
 }	
 
 void LSDCatchmentModel::save_data_and_draw_graphics()
