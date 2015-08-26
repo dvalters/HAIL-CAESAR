3f772093a7c20eb3c26e9f613c0a899f5ec04e61
s1143956 <DTMilodowski@users.noreply.github.com>
2015-08-26 11:27:05 +0000

    some LSDRaster stuff

--- LSDRaster.hpp
+++ LSDRaster.hpp
@@ -1941,6 +1941,14 @@ class LSDRaster
   /// @author SWDG
   /// @date 22/6/15
   LSDRaster RemoveBelow(float Value);
+
+  /// @brief apply a mask to a raster dataset, converts pixels to nodata where mas=k=1
+  /// @author DTM
+  /// @date 25/08/2015
+  LSDRaster apply_mask(LSDIndexRaster& mask);
+
+
+
   protected:
 
   ///Number of rows.
