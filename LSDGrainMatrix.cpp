7cea3959833a13c4c2b5b5aa679799f8e6a3d848
Declan Valters <declan.valters@gmail.com>
2016-06-15 17:28:52 +0000

    Fun afternoon spent battling pointers and references. Turned to drink early.

--- LSDGrainMatrix.cpp
+++ LSDGrainMatrix.cpp
@@ -34,14 +34,108 @@
 #include <cstdio>
 
 
-#include "LSDCatchmentModel.hpp"
+#include "LSDGrainMatrix.hpp"
 
-#ifndef LSDCatchmentModel_CPP
-#define LSDCatchmentModel_CPP
+#ifndef LSDGrainMatrix_CPP
+#define LSDGrainMatrix_CPP
 
-void LSDGrainMatrix::create()
+/*void LSDGrainMatrix::create()
 {
-  //
+  std::cout << "You are trying to create an LSDGrainMatrix object with no supplied files or parameters." << std::endl << "Exiting..." << std::endl;
+  exit(EXIT_FAILURE);
+}*/
+
+void LSDGrainMatrix::create(int imax, int jmax, int NoDataVal, int G_MAX)
+{
+  NRows = imax; // +2? -check in LSDCatchmentModel
+  NCols = jmax;
+  NoData = NoDataVal;
+  GrainFracMax = G_MAX;
+  std::cout << "Initialised a Grain Matrix..." << std::endl;
+}
+
+void LSDGrainMatrix::write_grainMatrix_to_ascii_file(std::string filename, 
+                                                     std::string fname_extension)
+{
+  std::string string_filename;
+  std::string dot = ".";
+  string_filename = filename + dot + fname_extension;
+  std::cout << "The graindata filename is: " << string_filename << std::endl;
+  
+  if (fname_extension == "asc")
+  {
+    // Open a grain data file
+    std::ofstream data_out(string_filename.c_str());
+    
+    if( data_out.fail() )
+    {
+      std::cout << "\n ERREUR FATALE!!!: Unable to write to " << string_filename \
+                   << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    
+    for(int i=1; i<=NRows; ++i)
+    {
+      for(int j=1; j<=NCols; ++j)
+      {
+        if (rasterIndex[i][j] != NoData)
+        {  
+          // Write the first part of the output file line (x,y location and index)
+          data_out << i << " " << j << " " << rasterIndex[i][j] << " ";
+          
+          // Now write the Surface Grain bit
+          for (int inc=0; inc<= GrainFracMax; inc++)
+          {
+            data_out << grainData[rasterIndex[i][j]][inc] << " ";
+          }
+          
+          // Now write the subsurface grain fractions
+          for(int z=0; z<=9; z++) // Loop through subsurface layers...
+          {
+            for(int inc=0; inc<=(GrainFracMax-2); inc++)
+            {
+              data_out << strataData[rasterIndex[i][j]][z][inc] << " ";
+            }
+          }
+          data_out << std::endl;
+        }
+        
+      } 
+    }
+    data_out.close();
+  }
+  
+  else
+  {
+    std::cout << "You did not enter and approprate extension!" << std::endl
+    << "You entered: " << fname_extension << ", options are: asc" << std::endl;
+    exit(EXIT_FAILURE);
+  }
 }
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- LSDGrainMatrix.hpp
+++ LSDGrainMatrix.hpp
@@ -2,54 +2,60 @@
 #define LSDGRAINMATRIX_HPP
 
 #include <string>
-#include <vector>
-#include <map>
+#include <fstream>
+#include <sstream>
 #include "TNT/tnt.h"
-//#include "LSDIndexRaster.hpp"
-//#include "LSDShapeTools.hpp"
 
 
+/// @brief This class is used primarily for the LSDCatchmentModel, to package up data
+/// about the stratigraphy and grain fraction data into neat objects. 
 class LSDGrainMatrix
 {
 public:
   
   friend class LSDRaster;
-  
+  /*
   LSDGrainMatrix()
   {
     create();  //throws an error -- we don't want an empty object
   }
-  
+  */
+  /*
   /// Create a GrainMatrix object by reading in a file
   LSDGrainMatrix(std::string fname, std::string fname_extension)
   {
     create(fname, fname_extension);
   }
-  
+  */
   /// Create a GrainMatrix object from references to arrays (in LSDCatchmentModel, though needn't be this object)
-  LSDGrainMatrix( TNT::Array2D<int>& indexes, 
+  LSDGrainMatrix( int imax, int jmax, int NoDataVal, int G_MAX,
+                  TNT::Array2D<int>& indexes, 
                   TNT::Array2D<double>& graindatas,
                   TNT::Array3D<double>& stratadatas)
+    : rasterIndex(indexes), grainData(graindatas), strataData(stratadatas)
   {
-    create(indexes, graindatas, stradatas);
+    create(imax, jmax, NoDataVal, G_MAX);
   }
   
   /// Writes the GrainMatrix object to an output text file (Warning: large file!)
   void write_grainMatrix_to_ascii_file(std::string filename, std::string fname_extension);
   
 protected:
-  TNT::Array2D<int> rasterIndex;
-  TNT::Array2D<double> grainData;
-  TNT::Array3D<double> strataData;
+  TNT::Array2D<int>& rasterIndex;
+  TNT::Array2D<double>& grainData;
+  TNT::Array3D<double>& strataData;
+  
+  int NCols;
+  int NRows;
+  int NoData;
+  int GrainFracMax;
   
 private:
-  void create();
-  void create(std::string fname, std::string fname_extension);
-  void create(TNT::Array2D<int>& indexes, 
-              TNT::Array2D<double>& graindatas,
-              TNT::Array3D<double>& stratadatas);
+  //void create();
+  //void create(std::string fname, std::string fname_extension);
+  void create(int imax, int jmax, int NoDataVal, int G_MAX);
   
-}
+};
     
 
 
