00ca80cb293109616cd70e3e0eaa6add31adf9b1
Declan Valters <declan.valters@gmail.com>
2017-08-06 19:39:04 +0100

    Namespace fix

--- src/catchmentmodel/LSDUtils.cpp
+++ src/catchmentmodel/LSDUtils.cpp
@@ -7,29 +7,32 @@
 #include <sys/stat.h>
 #include <catchmentmodel/LSDUtils.hpp>
 
-void quickOpenMPtest()
+namespace LSDUtils
 {
-  // test
-  #ifdef OMP_COMPILE_FOR_PARALLEL
-  int thread_id;
-  int num_threads = omp_get_max_threads();
-  int num_procs = omp_get_num_procs();
-
-  std::cout << "Hello! My name is OpenMP. I like to do LSD (Land Surface Dynamics) in parallel!" << std::endl;
-  std::cout << "Your system has: " << num_procs << " PROCESSORS available and " << num_threads << " THREADS to use!" << std::endl;
-  std::cout << "(Note: On some systems, threads are reported the same as processors.)" << std::endl;
-
-  #pragma omp parallel private ( thread_id ) num_threads (num_threads)
-  {
-    thread_id = omp_get_thread_num();
-
-    // So, you need a lock here (critical) because the << operator is
-    // not thread safe (i.e. you get race conditions)
-    #pragma omp critical
+    void quickOpenMPtest()
     {
-      std::cout << "Yo, whaddup! From thread number..." << thread_id << std::endl;
+      // test
+      #ifdef OMP_COMPILE_FOR_PARALLEL
+      int thread_id;
+      int num_threads = omp_get_max_threads();
+      int num_procs = omp_get_num_procs();
+
+      std::cout << "Hello! My name is OpenMP. I like to do LSD (Land Surface Dynamics) in parallel!" << std::endl;
+      std::cout << "Your system has: " << num_procs << " PROCESSORS available and " << num_threads << " THREADS to use!" << std::endl;
+      std::cout << "(Note: On some systems, threads are reported the same as processors.)" << std::endl;
+
+      #pragma omp parallel private ( thread_id ) num_threads (num_threads)
+      {
+        thread_id = omp_get_thread_num();
+
+        // So, you need a lock here (critical) because the << operator is
+        // not thread safe (i.e. you get race conditions)
+        #pragma omp critical
+        {
+          std::cout << "Yo, whaddup! From thread number..." << thread_id << std::endl;
+        }
+      }
+      std::cout << "Goodbye, parallel region!" << std::endl;
+      #endif
     }
-  }
-  std::cout << "Goodbye, parallel region!" << std::endl;
-  #endif
 }
