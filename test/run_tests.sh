27664f38abe687817ea3647418dc4461aaff0086
Declan Valters <declan.valters@gmail.com>
2018-06-30 20:07:46 +0100

    Added known good answers and mkdir in run_script

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+mkidr -p ./results/boscastle50m_72_u/
 # Boscastle test 1
 # 50m resolution, 72 hours rainfall
 ../bin/hailcaesar ./input_data/boscastle_input_data/ boscastle_test_72hr_50m_u.params
