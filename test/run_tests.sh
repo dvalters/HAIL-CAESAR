976a1717f3e236d220e72ef29ba1af425247a6b5
Declan Valters <declan.valters@gmail.com>
2018-06-30 16:31:04 +0100

    Fix some issues in the param files

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -2,4 +2,4 @@
 
 # Boscastle test 1
 # 50m resolution, 72 hours rainfall
-../bin/HAIL-CAESAR ./input_data/boscastle_input_data/ boscastle_input_data/boscastle_test_72hr_50m_u.params
+../bin/hailcaesar ./input_data/boscastle_input_data/ boscastle_test_72hr_50m_u.params
