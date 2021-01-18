e8f9050e01468c4d3a4ea4f90980bc516ceb76f5
Declan Valters <decval@bgs.ac.uk>
2021-01-18 11:30:55 +0000

    Folder structure tweak

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,5 +3,5 @@ mkdir -p ./results/boscastle50m_72_u/
 mkdir -p ./results/boscastle50m_72_u_flipped_lr/
 # Boscastle test 1
 # 50m resolution, 72 hours rainfall
-../bin/HAIL-CAESAR.exe ./input_data/boscastle_input_data/ boscastle_test_72hr_50m_u.params
-../bin/HAIL-CAESAR.exe ./input_data/boscastle_input_data/ boscastle_test_72hr_50m_u_flipped_lr.params
+../bin/HAIL-CAESAR.exe ./input_data/boscastle/boscastle_input_data/ boscastle_test_72hr_50m_u.params
+../bin/HAIL-CAESAR.exe ./input_data/boscastle/boscastle_input_data/ boscastle_test_72hr_50m_u_flipped_lr.params
